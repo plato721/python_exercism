5512a25feb3450273127e7d48e700c82b6f6aac2
Matt Ewell <matt.ewell@gmail.com>
2023-07-04 16:01:25 -0600

    Change inconsistent quotes

--- little-sisters-vocab/strings.py
+++ little-sisters-vocab/strings.py
@@ -1,4 +1,4 @@
-"""Functions for creating, transforming, and adding prefixes to strings."""
+'Functions for creating, transforming, and adding prefixes to strings.'
 import re
 
 ADJECTIVES = ['bright', 'dark', 'hard', 'soft',
@@ -6,12 +6,12 @@ ADJECTIVES = ['bright', 'dark', 'hard', 'soft',
 
 
 def add_prefix_un(word):
-    return "un" + word
+    return 'un' + word
 
 
 def make_word_groups(vocab_words):
     prefix = vocab_words[0]
-    join_sequence = " :: "
+    join_sequence = ' :: '
     prefixed_words = map(lambda word: prefix + word, vocab_words[1:])
 
     return prefix + join_sequence + join_sequence.join(prefixed_words)
@@ -23,17 +23,17 @@ def remove_suffix_ness(word):
 
 
 def remove_trailing_ness(word):
-    return re.sub(r"ness$", "", word)
+    return re.sub(r'ness$', '', word)
 
 
 def change_trailing_i_to_y(with_possible_i):
-    return re.sub(r"i$", "y", with_possible_i)
+    return re.sub(r'i$', 'y', with_possible_i)
 
 
 def adjective_to_verb(sentence, index):
     raw_adjective = word_at_index(index, sentence)
     cleaned_adjective = remove_periods(raw_adjective)
-    return cleaned_adjective + "en"
+    return cleaned_adjective + 'en'
 
 
 def word_at_index(index, sentence):
@@ -42,4 +42,4 @@ def word_at_index(index, sentence):
 
 
 def remove_periods(word):
-    return re.sub("[.]", "", word)
+    return re.sub('[.]', '', word)
