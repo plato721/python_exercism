c8e550211cd9533ba76cb2ee656d41c5fd63f042
Matt Ewell <matt.ewell@gmail.com>
2023-07-04 15:59:25 -0600

    Add initial little sister vocab solution

--- little-sisters-vocab/strings.py
+++ little-sisters-vocab/strings.py
@@ -1,54 +1,45 @@
 """Functions for creating, transforming, and adding prefixes to strings."""
+import re
+
+ADJECTIVES = ['bright', 'dark', 'hard', 'soft',
+              'light', 'damp', 'short', 'weak', 'black']
 
 
 def add_prefix_un(word):
-    """Take the given word and add the 'un' prefix.
-
-    :param word: str - containing the root word.
-    :return: str - of root word prepended with 'un'.
-    """
-
-    pass
+    return "un" + word
 
 
 def make_word_groups(vocab_words):
-    """Transform a list containing a prefix and words into a string with the prefix followed by the words with prefix prepended.
+    prefix = vocab_words[0]
+    join_sequence = " :: "
+    prefixed_words = map(lambda word: prefix + word, vocab_words[1:])
 
-    :param vocab_words: list - of vocabulary words with prefix in first index.
-    :return: str - of prefix followed by vocabulary words with
-            prefix applied.
-
-    This function takes a `vocab_words` list and returns a string
-    with the prefix and the words with prefix applied, separated
-     by ' :: '.
-
-    For example: list('en', 'close', 'joy', 'lighten'),
-    produces the following string: 'en :: enclose :: enjoy :: enlighten'.
-    """
-
-    pass
+    return prefix + join_sequence + join_sequence.join(prefixed_words)
 
 
 def remove_suffix_ness(word):
-    """Remove the suffix from the word while keeping spelling in mind.
+    with_possible_i = remove_trailing_ness(word)
+    return change_trailing_i_to_y(with_possible_i)
 
-    :param word: str - of word to remove suffix from.
-    :return: str - of word with suffix removed & spelling adjusted.
 
-    For example: "heaviness" becomes "heavy", but "sadness" becomes "sad".
-    """
+def remove_trailing_ness(word):
+    return re.sub(r"ness$", "", word)
 
-    pass
+
+def change_trailing_i_to_y(with_possible_i):
+    return re.sub(r"i$", "y", with_possible_i)
 
 
 def adjective_to_verb(sentence, index):
-    """Change the adjective within the sentence to a verb.
+    raw_adjective = word_at_index(index, sentence)
+    cleaned_adjective = remove_periods(raw_adjective)
+    return cleaned_adjective + "en"
 
-    :param sentence: str - that uses the word in sentence.
-    :param index: int - index of the word to remove and transform.
-    :return: str - word that changes the extracted adjective to a verb.
 
-    For example, ("It got dark as the sun set", 2) becomes "darken".
-    """
+def word_at_index(index, sentence):
+    split_sentence = sentence.split()
+    return split_sentence[index]
 
-    pass
+
+def remove_periods(word):
+    return re.sub("[.]", "", word)
