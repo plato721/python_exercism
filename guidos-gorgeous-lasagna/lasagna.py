343a4e85e881163a5e0301370786e6b4dd46679b
Matt Ewell <matt.ewell@gmail.com>
2023-07-03 22:17:09 -0600

    Add solved Guido's Lasagna

--- guidos-gorgeous-lasagna/lasagna.py
+++ guidos-gorgeous-lasagna/lasagna.py
@@ -7,29 +7,38 @@ This is a module docstring, used to describe the functionality
 of a module and its functions and/or classes.
 """
 
+EXPECTED_BAKE_TIME = 40
 
-#TODO: define the 'EXPECTED_BAKE_TIME' constant.
 
-
-#TODO: Remove 'pass' and complete the 'bake_time_remaining()' function below.
-def bake_time_remaining():
+def bake_time_remaining(bake_time_elapsed):
     """Calculate the bake time remaining.
 
-    :param elapsed_bake_time: int - baking time already elapsed.
+    :param bake_time_elapsed: int - baking time already elapsed.
     :return: int - remaining bake time (in minutes) derived from 'EXPECTED_BAKE_TIME'.
 
     Function that takes the actual minutes the lasagna has been in the oven as
     an argument and returns how many minutes the lasagna still needs to bake
     based on the `EXPECTED_BAKE_TIME`.
     """
-
-    pass
-
-
-#TODO: Define the 'preparation_time_in_minutes()' function below.
-# You might also consider using 'PREPARATION_TIME' here, if you have it defined.
+    return EXPECTED_BAKE_TIME - bake_time_elapsed
 
 
+def preparation_time_in_minutes(num_layers):
+    """Calculate the prep time. In minutes.
 
-#TODO: define the 'elapsed_time_in_minutes()' function below.
-# Remember to add a docstring (you can copy and then alter the one from bake_time_remaining.)
+    :param num_layers: int
+    :return: int - prep time for number of layers provided
+    """
+    return num_layers * 2
+
+
+def elapsed_time_in_minutes(num_layers, already_elapsed):
+    """Calculate how much time a lasagna has been baking and being prepared, given
+    the number of layers and time that has elapsed in the oven. Do I feel a bit
+    sheepish writing this? I do.
+
+    :param num_layers: int
+    :param already_elapsed: int
+    :return: int
+    """
+    return preparation_time_in_minutes(num_layers) + already_elapsed
